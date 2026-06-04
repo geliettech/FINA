4d5056e489c72d7919c5e57de645299874ac76f0
julietoge <julietogechi27@gmail.com>
2026-06-04 16:48:29 +0100

    fix destructure error

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,5 +1,16 @@
-export const useGetUserInfo = () => {
-    const {name, profilePic, userID, isAuth} = JSON.parse(localStorage.getItem("auth"))
+// export const useGetUserInfo = () => {
+//     const {name, profilePic, userID, isAuth} = JSON.parse(localStorage.getItem("auth"))
 
-    return {name, profilePic, userID, isAuth}
-}
+//     return {name, profilePic, userID, isAuth}
+// }
+
+export const useGetUserInfo = () => {
+  const userInfo = JSON.parse(localStorage.getItem("auth") || "null");
+
+  return {
+    name: userInfo?.name,
+    profilePic: userInfo?.profilePic,
+    userID: userInfo?.userID,
+    isAuth: userInfo?.isAuth ?? false,
+  };
+};
