14241b42f40f8f62788caa20f6a685a1995afbb3
julietoge <julietogechi27@gmail.com>
2026-06-27 17:21:24 +0100

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -28,35 +28,25 @@ const Header = () => {
 
 
   return (
-    <header className={clsx("fixed top-0 left-0 w-full z-50 text-foreground transition-all duration-500 py-10 max-lg:py-4", hasScrolled
+    <header className={clsx("fixed top-0 left-0 z-50 w-full text-foreground transition-all duration-500", hasScrolled
           ? "py-2 bg-background/70 backdrop-blur-md shadow-sm"
-          : "bg-transparent")}>
-        <div className="container flex items-center justify-between h-18">
+          : "py-6 max-lg:py-4 bg-transparent")}>
+        <div className="container flex items-center justify-between h-16">
         {/* logo */}
-        <Link to="/" className="cursor-pointer z-2">
+        <Link to="/" className="relative z-20 cursor-pointer">
           <img src={
             theme === "light"
               ? "/images/fina-light.png"
               : "/images/fina-dark.png"
           } width={160} height={55} alt="logo" />
         </Link>
-         <div className={clsx("w-full max-lg:absolute max-lg:top-full max-lg:left-5 max-lg:right-5 max-lg:bg-background max-lg:shadow-lg max-lg:overflow-hidden transition-all duration-300 max-lg:bg-s2",
-         isOpen ? "max-lg:max-h-[600px] max-lg:opacity-100 max-lg:translate-y-0" : "max-lg:max-h-0 max-lg:opacity-0 pointer-events-none max-lg:-translate-y-5",)}>
-   
-    <div className="relative
-    flex
-    items-center
-    justify-between
-    w-full
-
-    max-lg:flex-col
-    max-lg:items-center
-    max-lg:justify-center
-    max-lg:py-10
-    max-lg:gap-2">
+   <div className={clsx("lg:flex lg:flex-1 lg:items-center lg:justify-between lg:ml-2","max-lg:absolute max-lg:top-full max-lg:left-6 max-lg:right-6 max-lg:bg-s1 max-lg:rounded-xl max-lg:shadow-xl", "transition-all duration-300 ",
+         isOpen ? "max-lg:opacity-100 max-lg:translate-y-0" : "max-lg:opacity-0 max-lg:-translate-y-5 pointer-events-none ")}>
+          {/* navigation & authenication */}
+    <div className="relative z-10 flex lg:flex-row flex-col lg:items-center lg:justify-between w-full max-lg:p-10 max-lg:gap-8">
           {/* navigation */}
-          <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
-            <ul className="flex max-lg:block max-lg:gap-6 gap-12">
+          <nav className="w-full lg:w-auto">
+            <ul className="flex max-lg:flex-col gap-12">
               {NAV_ITEMS.map((title) => (
                 <li key={title} className="nav-li">
                   <LinkScroll
@@ -66,9 +56,9 @@ const Header = () => {
                     spy
                     smooth
                     activeClass="nav-active"
-                    className="base-bold text-foreground capitalized transition-colors duration-500 cursor-pointer hover:text-s1 max-lg:py-4 max-lg:h5 transition-all
-duration-300
-hover:scale-110"
+                    className="base-bold text-foreground capitalized cursor-pointer max-lg:h5 transition-all
+duration-500 
+hover:scale-105 hover:text-s3 max-lg:hover:text-p1"
                   >
                     {title}
                   </LinkScroll>
@@ -77,8 +67,10 @@ hover:scale-110"
             </ul>
           </nav>
           {/* authenication */}
-          <div className="flex flex-col lg:flex-row gap-4">
-            <button onClick={() => navigate("/sign-in")} className="nav-li">
+          <div className="flex flex-col lg:flex-row gap-8">
+            <button onClick={() => navigate("/sign-in")} className="nav-li base-bold text-foreground capitalized cursor-pointer max-lg:h5 transition-all
+duration-500
+hover:scale-105 hover:text-s3 max-lg:hover:text-p1">
               Sign In
             </button>
 
@@ -86,29 +78,26 @@ hover:scale-110"
               Start for Free
             </button>
           </div>
-          <div className="lg:hidden block absolute top-1/2 left-0 w-960 h-95 -translate-x-72.5 -translate-y-1/2 rotate-90">
-            <img
-              src="/images/bg-outlines.svg"
-              width={960}
-              height={380}
-              alt="outline"
-              className="relative z-2"
-            />
-            <img
-              src="/images/bg-outlines-fill.png"
-              width={960}
-              height={380}
-              alt="outline"
-              className="absolute inset-0 mix-blend-soft-light opacity-5"
-            />
-          </div>
         </div>
-      
+         {/* Background */}
+           <div className="lg:hidden absolute inset-0 pointer-events-none">
+            <img
+  src="/images/bg-outlines.svg"
+  alt=""
+  className="absolute inset-0 w-full h-full object-cover"
+/>
+
+<img
+  src="/images/bg-outlines-fill.png"
+  alt=""
+  className="absolute inset-0 w-full h-full object-cover mix-blend-soft-light opacity-5"
+/>
+          </div>
         </div>
         {/* Hamburger */}
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
-          onClick={() => setIsOpen((prevState) => !prevState)}
+          onClick={() => setIsOpen((prevState) => !prevState)} aria-label="Toggle menu"
         >
           <img
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
